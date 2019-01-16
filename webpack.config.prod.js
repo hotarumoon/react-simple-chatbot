268e95a3bcb9c8a65d70a388eb239d117178649c
Ege <egeyag@gmail.com>
2019-01-16 17:26:04 +0300

    Add files via upload

---
+++ webpack.config.prod.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'lib/index'),
+  externals: { 'styled-components': 'styled-components' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'react-simple-chatbot.js',
+    publicPath: 'dist/',
+    library: 'ReactSimpleChatbot',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new UglifyJsPlugin({
+      comments: false,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+};
